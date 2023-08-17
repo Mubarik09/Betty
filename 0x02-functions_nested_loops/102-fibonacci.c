0f5fd3b2b3eed4297b43509ae8b3b05629399358
Mubarik <mubarikmustapha7@gmail.com>
2023-08-16 18:09:56 -0700

    fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-int fib1 = 1, fib2 = 2, next;
+unsigned long fib1 = 1, fib2 = 2, next;
 int count;
 
-printf("%d, %d", fib1, fib2);
+printf("%lu, %lu", fib1, fib2);
 
 for (count = 2; count < 50; count++)
 {
 next = fib1 + fib2;
-printf(", %d", next);
+printf(", %lu", next);
 fib1 = fib2;
 fib2 = next;
 }
 
-printf("\n");
+putchar('\n');
 
 return (0);
 }
+
 
