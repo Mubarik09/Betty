da4d269e4b67a8405c538bfdea56ca8369410918
Mubarik <mubarikmustapha7@gmail.com>
2023-09-06 15:43:05 -0700

    we start den we end

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+puts("\"Programming is like building a multilingual puzzle");
+}
+
