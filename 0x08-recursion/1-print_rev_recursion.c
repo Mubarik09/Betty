65b844e982db3b5f3aff8a85469700a1484eb089
Mubarik09 <mubarikmustapha7@gmail.com>
2023-10-26 15:27:25 -0700

    changes to previous string reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,14 +7,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-else
+if (*s)
 {
 _print_rev_recursion(s + 1);
 _putchar(*s);
+}
+else
+{
+return;
 }
 }
