9a6aa74f360db2d61095aaf27b057406a13a8be3
Mubarik09 <mubarikmustapha7@gmail.com>
2023-10-27 22:45:43 -0700

    added comments to task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints its name followed by a new line.
+ * If you rename the program, it will print the new name without
+ * requiring recompilation. The path before the program name is not removed.
+ *@argc: unused parameter.
+ * @argv: array of cli arguments
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 (void)argc;
