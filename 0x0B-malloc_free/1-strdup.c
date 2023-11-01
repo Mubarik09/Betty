fc95fdb68c9228376bc21b2a1c2c7dec54f62719
Mubarik09 <mubarikmustapha7@gmail.com>
2023-10-31 21:17:10 -0700

    assignment 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,19 @@
+/**
+ * string_length - Calculate the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string (number of characters).
+ */
+int len(char *str) 
+{
+int length = 0;
+while (str[length] != '\0') 
+{
+length++;
+}
+return length;
+}
+
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,8 +25,8 @@
 char *_strdup(char *str)
 {
 int i = 0;
-int size = (len(str)) + 1;
-char *array = malloc(size * sizeof(*array));
+int size = len(str) + 1;
+char *array =malloc(size * sizeof(*array));
 if (array == NULL)
 {
 return (0);
