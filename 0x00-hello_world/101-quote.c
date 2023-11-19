654d97c5781cf92e22e76a81052635165888a80e
Mubarik09 <mubarikmustapha7@gmail.com>
2023-11-19 05:21:49 -0800

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - A program that prints a line to the standard error
  * Return 1 (Success)
  */
-main(void)
+int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, sizeof(str));
 return (1);
 }
