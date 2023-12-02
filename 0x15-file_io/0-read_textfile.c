5587d8c62472256674670daff304031b9099a860
Mubarik09 <mubarikmustapha7@gmail.com>
2023-12-01 22:19:50 -0800

    updatefile I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,20 +8,29 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fptr = open(filename, O_RDONLY);
-int count;
-char *str = (char *)malloc(letters * sizeof(char));
+int count, total_written;
+char *str;
 if (filename == NULL)
 return (0);
 
 if (fptr == -1)
 return (0);
+str = (char *)malloc(letters * sizeof(char));
 count = read(fptr, str, letters);
 if (count == -1)
 {
+free(str);
 close(fptr);
 return (0);
 }
-dprintf(1, "%s", str);
+total_written = write(1, str, count);
+if (total_written != count)
+{
+free(str);
+close(fptr);
+return (0);
+}
+free(str);
 close(fptr);
 return (count);
 }
